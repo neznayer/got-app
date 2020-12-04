c5bb551afd87043424375dccbeb3cb3cb9edc8e7
Anton <nokker@gmail.com>
2020-12-04 23:03:30 +0900

    added build

---
+++ build/precache-manifest.4c4b106aa337b370b27a0631d079a91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a21e49c8638a033b46a",
+    "url": "./static/css/main.1be4783d.chunk.css"
+  },
+  {
+    "revision": "7a21e49c8638a033b46a",
+    "url": "./static/js/main.7a21e49c.chunk.js"
+  },
+  {
+    "revision": "b6338524c2ae7059158b",
+    "url": "./static/css/1.02d51343.chunk.css"
+  },
+  {
+    "revision": "b6338524c2ae7059158b",
+    "url": "./static/js/1.b6338524.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "40f1f14a159a2c065a51e94dd2879c98",
+    "url": "./index.html"
+  }
+];
